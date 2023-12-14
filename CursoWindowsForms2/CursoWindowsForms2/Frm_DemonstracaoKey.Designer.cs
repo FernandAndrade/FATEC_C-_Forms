46611bfbc1cb6db975d548bb3bc2b9910b8a28d1
Fernanda Andrade <andrade97.fernanda@gmail.com>
2023-12-14 19:31:23 -0300

    :tada: Curso C# Forms - Alura

---
+++ CursoWindowsForms2/CursoWindowsForms2/Frm_DemonstracaoKey.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace CursoWindowsForms2
+{
+    partial class Frm_DemonstracaoKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_DemonstracaoKey));
+            this.Txt_Input = new System.Windows.Forms.TextBox();
+            this.txt_Msg = new System.Windows.Forms.TextBox();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Lbl_Minus = new System.Windows.Forms.Label();
+            this.Lbl_maius = new System.Windows.Forms.Label();
+            this.lbl_Upper = new System.Windows.Forms.Label();
+            this.Lbl_Lower = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Txt_Input
+            // 
+            this.Txt_Input.Location = new System.Drawing.Point(8, 8);
+            this.Txt_Input.Name = "Txt_Input";
+            this.Txt_Input.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Input.TabIndex = 0;
+            this.Txt_Input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_Input_KeyDown);
+            // 
+            // txt_Msg
+            // 
+            this.txt_Msg.Location = new System.Drawing.Point(8, 40);
+            this.txt_Msg.Multiline = true;
+            this.txt_Msg.Name = "txt_Msg";
+            this.txt_Msg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Msg.Size = new System.Drawing.Size(304, 232);
+            this.txt_Msg.TabIndex = 1;
+            this.txt_Msg.TabStop = false;
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Location = new System.Drawing.Point(328, 8);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(75, 28);
+            this.Btn_Reset.TabIndex = 2;
+            this.Btn_Reset.Text = "LIMPA";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_Reset_Click);
+            // 
+            // Lbl_Minus
+            // 
+            this.Lbl_Minus.AutoSize = true;
+            this.Lbl_Minus.Location = new System.Drawing.Point(320, 103);
+            this.Lbl_Minus.Name = "Lbl_Minus";
+            this.Lbl_Minus.Size = new System.Drawing.Size(35, 13);
+            this.Lbl_Minus.TabIndex = 3;
+            this.Lbl_Minus.Text = "Maius";
+            // 
+            // Lbl_maius
+            // 
+            this.Lbl_maius.AutoSize = true;
+            this.Lbl_maius.Location = new System.Drawing.Point(320, 56);
+            this.Lbl_maius.Name = "Lbl_maius";
+            this.Lbl_maius.Size = new System.Drawing.Size(35, 13);
+            this.Lbl_maius.TabIndex = 4;
+            this.Lbl_maius.Text = "Minus";
+            // 
+            // lbl_Upper
+            // 
+            this.lbl_Upper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_Upper.Location = new System.Drawing.Point(368, 56);
+            this.lbl_Upper.Name = "lbl_Upper";
+            this.lbl_Upper.Size = new System.Drawing.Size(32, 23);
+            this.lbl_Upper.TabIndex = 5;
+            // 
+            // Lbl_Lower
+            // 
+            this.Lbl_Lower.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl_Lower.Location = new System.Drawing.Point(368, 104);
+            this.Lbl_Lower.Name = "Lbl_Lower";
+            this.Lbl_Lower.Size = new System.Drawing.Size(32, 23);
+            this.Lbl_Lower.TabIndex = 6;
+            // 
+            // Frm_DemonstracaoKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 281);
+            this.Controls.Add(this.Lbl_Lower);
+            this.Controls.Add(this.lbl_Upper);
+            this.Controls.Add(this.Lbl_maius);
+            this.Controls.Add(this.Lbl_Minus);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.txt_Msg);
+            this.Controls.Add(this.Txt_Input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_DemonstracaoKey";
+            this.Text = "Demonstração Evento Key";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Input;
+        private System.Windows.Forms.TextBox txt_Msg;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Label Lbl_Minus;
+        private System.Windows.Forms.Label Lbl_maius;
+        private System.Windows.Forms.Label lbl_Upper;
+        private System.Windows.Forms.Label Lbl_Lower;
+    }
+}
---
+++ CursoWindowsForms2/CursoWindowsForms2/Frm_DemonstracaoKey.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms2
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void Txt_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            txt_Msg.AppendText("\r\n" + "Pressionei uma tecla: " + e.KeyCode + "\r\n");
+            txt_Msg.AppendText("\t" + "Código da Tecla: " + ((int)e.KeyCode) + "\r\n");
+            txt_Msg.AppendText("\t" + "Nome da tecla: " + e.KeyData + "\r\n");
+            Lbl_Lower.Text = e.KeyCode.ToString().ToLower();
+            lbl_Upper.Text = e.KeyCode.ToString().ToUpper();
+            
+
+
+
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            txt_Msg.Text = " ";
+            Txt_Input.Text = " ";
+            lbl_Upper.Text = " ";
+            Lbl_Lower.Text = " ";
+
+
+
+        }
+    }
+}
